71aa709e86de52f71c2a8f1e16f7bf69cb4c1263
Rich Cross <richwesley@comcast.net>
2017-09-18 13:53:17 -0700

    Separated the NavMenu from Main into new file Menu

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,23 +1,12 @@
 import React, {Component} from 'react'
-
+import Menu from './Menu'
 import {Link} from 'react-router'
 class Main extends Component{
  
       render() {
         return (
-          <div className="container-fluid" >
-
-            <nav className="navbar-toggler nav navbar-default">  
-                <div className="navbar-toggler">
-                        <ul className="nav navbar-nav navbar-right"> 
-                          <li><Link to="/dashboard"><span className="teal glyphicon glyphicon-globe"></span> Dashboard</Link></li>
-                          <li><Link to="/"><span className="glyphicon glyphicon-log-out"></span> Logout </Link></li>
-                          <li><Link to ="/login"><span className="glyphicon glyphicon-log-in"></span> Login</Link></li>
-                          <li><Link to ="/register"><span className="glyphicon glyphicon-user"></span> Register</Link></li>
-                        </ul>
-                </div>        
-        </nav>
-
+          <div className="container-fluid main" >
+              <Menu />
 
     <div className="container-fluid">
         <br />
