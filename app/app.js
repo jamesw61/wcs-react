d3eeafc24cacc801cc6328c0e2e746dc3dadad37
James Wick <jmwick2@hotmail.com>
2017-09-18 22:49:28 -0700

    app.js

--- app/app.js
+++ app/app.js
@@ -10,10 +10,5 @@ var routerRoutes = require("./config/routerRoutes");
 // This is where the routes will be rendered
 
 // // Renders the contents according to the route page.
-<<<<<<< HEAD
 
-ReactDOM.render(routes, app);
-
-=======
 ReactDOM.render(routerRoutes, document.getElementById('app'));
->>>>>>> d21d09d49fbaf7c6acb9918c0f29143db2d12060
