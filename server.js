afc61107344e3c10da67fade66625a36266b0158
Frank Mcdonald <Frankmcd@cox.net>
2017-09-23 10:55:49 -0700

    update

--- server.js
+++ server.js
@@ -78,10 +78,12 @@ app.use('/users', users);
 app.use('/contests', contests);
 app.use('/dancers', dancers);
 
-mongoose.connect("mongodb://dinoman:UACodingB00tcamp@ds139904.mlab.com:39904/wcs", {
-  useMongoClient: true
-});
-
+// mongoose.connect("mongodb://dinoman:UACodingB00tcamp@ds139904.mlab.com:39904/wcs", {
+//   useMongoClient: true
+// });
+mongoose.connect("mongodb://localhost/wcs", {
+	useMongoClient: true
+  });
 
 var db = mongoose.connection;
 
