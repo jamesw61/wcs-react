47b8b7e3c429c20c53271f1be475651c213ed7cc
Rich Cross <richwesley@comcast.net>
2017-09-25 15:57:52 -0700

    Even more Visual Enhancements

--- app/components/Home.js
+++ app/components/Home.js
@@ -4,9 +4,24 @@ class Home extends Component{
  
       render() {
         return (
-          <div className="container-fluid" >
-            <img src="./css/JnJ3.png" className="img-responsive logo" alt="logo" />      
-          </div>
+          <div className="container" >
+            <div className="row">
+              <div className="col-md-1"></div>
+              <div className="col-md-4">
+                <img src="./css/JnJ3.png" className="img-responsive logo" alt="logo" />
+              </div>
+              <div className="col-md-7">
+                <h1 className="home">West Coast Swing</h1>
+              </div>
+              
+              </div>
+              
+              <div className="row silhouette">
+                <div className="col-md-12 swing">            
+                  <img src="../images/swing-dancers-rev.png" className="img-responsive" alt="silhouette" />
+                </div>
+              </div>
+            </div>
         )}   
     }
     
