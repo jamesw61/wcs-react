7138fcc4aba6237dfcca8e685524204bc2a49744
Rich Cross <richwesley@comcast.net>
2017-09-27 21:21:15 -0700

    crud

---
+++ app/components/editParticipant.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class EditItem extends Component {
+
+  constructor(props) {
+      super(props);
+      this.state = {};
+  }
+
+  componentDidMount(){
+    axios.get('http://localhost:4200/items/edit/'+this.props.match.params.id)
+    .then(response => {
+      this.setState({ items: response.data });
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+  }
+
+  render() {
+    return (
+          <div className="container">
+            <form onSubmit={this.handleSubmit}>
+              <label>
+                Edit Item:
+                <input type="text" value={this.state.items.item}  className="form-control"/>
+              </label><br/>
+              <input type="submit" value="Update" className="btn btn-primary"/>
+            </form>
+        </div>
+    );
+  }
+}
